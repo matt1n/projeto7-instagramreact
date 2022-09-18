f0741613b88f846e0b9a8dfc533ba33b45ee6a03
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-09-18 17:29:32 -0300

    Icones ficando preenchidos e dados de usuario sendo trocados

--- src/Posts.js
+++ src/Posts.js
@@ -1,49 +1,85 @@
-function Post(props) {
-  return (
-    <div class="post">
-      <div class="topo">
-        <div class="usuario">
-          <img src={props.imagemUsuario} />
-          {props.nomeUsuario}
-        </div>
-        <div class="acoes">
-          <ion-icon name="ellipsis-horizontal"></ion-icon>
-        </div>
-      </div>
-
-      <div class="conteudo">
-        <img src={props.imagemConteudo} />
-      </div>
-
-      <div class="fundo">
-        <div class="acoes">
-          <div>
-            <ion-icon name="heart-outline"></ion-icon>
-            <ion-icon name="chatbubble-outline"></ion-icon>
-            <ion-icon name="paper-plane-outline"></ion-icon>
-          </div>
-          <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
-          </div>
-        </div>
-
-        <div class="curtidas">
-          <img src={props.imagemCurtida} />
-          <div class="texto">
-            Curtido por <strong>{props.nomeCurtida}</strong> e{" "}
-            <strong>outras {props.numeroCurtidas} pessoas</strong>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 export default function Posts() {
   const informacoesPosts = [
-    {imagemUsuario:"assets/img/meowed.svg", nomeUsuario: "meowed", imagemConteudo: "assets/img/gato-telefone.svg", imagemCurtida: "assets/img/respondeai.svg", nomeCurtida: "respondeai", numeroCurtidas: "101.523"},
-    {imagemUsuario:"assets/img/barked.svg", nomeUsuario: "barked", imagemConteudo: "assets/img/dog.svg", imagemCurtida: "assets/img/adorable_animals.svg", nomeCurtida: "adorable_animals", numeroCurtidas: "99.159"}
+    {imagemUsuario:"assets/img/meowed.svg", nomeUsuario: "meowed", imagemConteudo: "assets/img/gato-telefone.svg", imagemCurtida: "assets/img/respondeai.svg", nomeCurtida: "respondeai", numeroCurtidas: "101523"},
+    {imagemUsuario:"assets/img/barked.svg", nomeUsuario: "barked", imagemConteudo: "assets/img/dog.svg", imagemCurtida: "assets/img/adorable_animals.svg", nomeCurtida: "adorable_animals", numeroCurtidas: "99159"}
   ]
+
+  function Post(props) {
+    const [salvo, setSalvo] = React.useState("bookmark-outline")
+    const [curtida, setCurtida] = React.useState("heart-outline")
+    const [classeCurtida, setClasseCurtida] = React.useState("")
+    const quantidadeCurtidas=Number(props.numeroCurtidas)
+    const [numCurtidas, setNumCurtidas] = React.useState(quantidadeCurtidas.toLocaleString('pt-BR'))
+
+    function verificaSalvo(){
+      if (salvo === "bookmark-outline"){
+        setSalvo("bookmark")
+      } else {
+        setSalvo("bookmark-outline")
+      }
+    }
+    
+    function verificaCurtidaImagem(){
+      if (curtida === "heart-outline"){
+        setCurtida("heart")
+        setClasseCurtida("vermelho")
+        setNumCurtidas((quantidadeCurtidas+1).toLocaleString('pt-BR'))
+      }
+    }
+
+    function verificaCurtida(){
+      if (curtida === "heart-outline"){
+        setCurtida("heart")
+        setClasseCurtida("vermelho")
+        setNumCurtidas((quantidadeCurtidas+1).toLocaleString('pt-BR'))
+      } else {
+        setCurtida("heart-outline")
+        setClasseCurtida("")
+        setNumCurtidas((quantidadeCurtidas).toLocaleString('pt-BR'))
+      }
+    }
+    
+    return (
+      <div class="post">
+        <div class="topo">
+          <div class="usuario">
+            <img src={props.imagemUsuario} />
+            {props.nomeUsuario}
+          </div>
+          <div class="acoes">
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+  
+        <div class="conteudo">
+          <img onClick={verificaCurtidaImagem} src={props.imagemConteudo} />
+        </div>
+  
+        <div class="fundo">
+          <div class="acoes">
+            <div>
+              <ion-icon class={classeCurtida} onClick={verificaCurtida} name={curtida}></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div>
+              <ion-icon onClick={verificaSalvo} name={salvo}></ion-icon>
+            </div>
+          </div>
+  
+          <div class="curtidas">
+            <img src={props.imagemCurtida} />
+            <div class="texto">
+              Curtido por <strong>{props.nomeCurtida}</strong> e{" "}
+              <strong>outras {numCurtidas} pessoas</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   
   return (
     <div class="posts">
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,26 +1,40 @@
-function ExibeUsuario(props) {
-  return (
-    <>
-      <img src={props.imagem} />
-      <div class="texto">
-        <strong>{props.nomeUsuario}</strong>
-        <span>
-          {props.nome}
-          <ion-icon name="pencil"></ion-icon>
-        </span>
-      </div>
-    </>
-  );
-}
+import React from "react";
 
 export default function Usuario() {
+  
+  const [nome, setNome] = React.useState('Catana')
+  const [imagem, setImagem] = React.useState('assets/img/catanacomics.svg')
+  
+  function mudaNome(){
+    setNome(prompt("Digite seu novo nome de usuário"))
+  }
+  
+  function mudaImagem(){
+    setImagem(prompt("DIgite o endereço de sua nova imagem"))
+  }
+
+  function ExibeUsuario(props) {
+    return (
+      <>
+        <img onClick={mudaImagem} src={props.imagem} />
+        <div class="texto">
+          <strong>{props.nomeUsuario}</strong>
+          <span>
+            {props.nome}
+            <ion-icon onClick={mudaNome} name="pencil"></ion-icon>
+          </span>
+        </div>
+      </>
+    );
+  }
+  
   return (
     <div class="usuario">
       {
         <ExibeUsuario
-          imagem="assets/img/catanacomics.svg"
-          nomeUsuario="catanacomics<"
-          nome="Catana"
+          imagem={imagem}
+          nomeUsuario="catanacomics"
+          nome={nome}
         />
       }
     </div>
